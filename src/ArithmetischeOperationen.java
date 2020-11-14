4b574863eb2afa31cde9dbf437c2e3db5b245fcb
Lukas Reisinger <hello@lukasreisinger.com>
2020-11-14 11:30:09 +0100

    Var showcases in demofile,new class "arithmetische operationen" with mathematical operations

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,12 @@
+public class ArithmetischeOperationen {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 5;
+
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println(a + " - " + b + " = " + (a - b)) ;
+        System.out.println(a + " * " + b + " = " + (a * b)) ;
+        System.out.println(a + " / " + b + " = " + (a / b)) ;
+        System.out.println(a + " % " + b + " = " + (a % b)) ;
+    }
+}
--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,24 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("PR1 per Fernlehre");
+        System.out.println(1234);
+        System.out.println(false);
+        System.out.println(2.17);
+        System.out.println('X');
+
+        boolean Bool= true;
+        System.out.println("Bool = " + Bool);
+        
+        Bool = false;
+        System.out.println("Bool = " + Bool);
+
+        String text = "Hello Mello";
+        short smallNumber = 1111;
+        int bigNumber = 12333;
+
+        System.out.println(text);
+        System.out.println(smallNumber + bigNumber);
+
     }
 
 }
